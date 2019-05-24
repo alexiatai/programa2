420c464e3d691aa8966b1aa474675df128e092b7
alexiatai <47820420+alexiatai@users.noreply.github.com>
2019-05-24 11:04:18 -0300

    Add files via upload

--- teste_classes.py
+++ teste_classes.py
@@ -4,9 +4,9 @@ class Produto():
         self.preco = preco
 
 
-class Item(Produto):
+class Item():
     def __init__(self, produto, quantidade):
-        self.produto = Produto(nome, preco)
+        self.produto = produto
         self.quantidade = quantidade
 
 
@@ -14,20 +14,7 @@ class Cestinha():
     def __init__(self, itens):
         self.itens = itens
 
-class Caixa():
-    def __init__(self, data, hora, produto, itens):
-        self.data = data
-        self.hora = hora
-        self.produto = Produto(self,nome,preco)
-        self.itens = Item(self,produto,qtd)
 
-    def calcular_preco(self, preco ,quantidade):
-        preco_prod = preco*quantidade
-        self.itens.append(preco_prod)
-        return preco_prod
 
-    def calcular_total(self, itens):
-        valor_total = sum(self.itens)
-        return valor_total
 
         
